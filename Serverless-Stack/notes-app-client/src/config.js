5ea6bb92a77456986d0e796131cbee82f5b37c4d
YongHoonJJo <starctak@gmail.com>
2019-12-24 22:12:53 +0900

    [notes-app-client] Configure AWS Amplify

---
+++ Serverless-Stack/notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-ex"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://ultn4xzava.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_ybV8OqaR6",
+    APP_CLIENT_ID: "3i89jq5fh94domkler42rnr1mq",
+    IDENTITY_POOL_ID: "us-east-1:c0c1079f-5422-46bd-bc1b-d38e7ab3bfa9"
+  }
+};
--- Serverless-Stack/notes-app-client/src/index.js
+++ Serverless-Stack/notes-app-client/src/index.js
@@ -5,6 +5,33 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
+import Amplify from "aws-amplify";
+import config from "./config";
+
+Amplify.configure({ // 상호 작용하고자하는 다양한 AWS 리소스를 설정하는 것
+  Auth: { // Cognito
+    mandatorySignIn: true, // 사용자들이 앱과 상호 작용하기 전에 로그인해야 하기 때문
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  Storage: { // S3
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID
+  },
+  API: { // API Gateway
+    endpoints: [
+      {
+        name: "notes", // Amplify에 기본적으로 우리의 API의 이름을 지정하도록하고 있다.
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      },
+    ]
+  }
+});
+
 ReactDOM.render(
   <Router>
     <App />
