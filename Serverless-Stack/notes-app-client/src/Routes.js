25c343e311c6e5cf39801cb070efc3d3e730577b
YongHoonJJo <starctak@gmail.com>
2019-12-27 18:15:07 +0900

    [notes-app-client] Create a Login Page

--- Serverless-Stack/notes-app-client/src/Routes.js
+++ Serverless-Stack/notes-app-client/src/Routes.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
+import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
 
-export default () =>
-  <Switch>
-    <Route path="/" exact component={Home} />
-    <Route component={NotFound} />
-  </Switch>;
+export default function Routes({ appProps }) {
+  return (
+    <Switch>
+      <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+      <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      { /* Finally, catch all unmatched routes */ }
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+// export default () =>
+//   <Switch>
+//     <Route path="/" exact component={Home} />
+//     <Route path="/login" exact component={Login} />
+//     <Route component={NotFound} />
+//   </Switch>;
--- Serverless-Stack/notes-app-client/src/config.js
+++ Serverless-Stack/notes-app-client/src/config.js
@@ -11,6 +11,6 @@ export default {
     REGION: "us-east-1",
     USER_POOL_ID: "us-east-1_ybV8OqaR6",
     APP_CLIENT_ID: "3i89jq5fh94domkler42rnr1mq",
-    IDENTITY_POOL_ID: "us-east-1:c0c1079f-5422-46bd-bc1b-d38e7ab3bfa9"
+    IDENTITY_POOL_ID: "us-east-1:ed8c763e-05be-43c4-a35d-1bd571142301"
   }
 };
